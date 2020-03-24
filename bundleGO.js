2f7ed5b4e8ffe5068c77c34083dd3cbba7ace26d
Leo karl Ladeno <leo.ladeno@growthops.com.au>
2020-03-24 14:20:46 +0800

    floatHead width to 100%

--- bundleGO.js
+++ bundleGO.js
@@ -39966,6 +39966,7 @@
 						setTimeout(function(){
 							var newWidth = table.closest(".table-scroll-container").width();
 							tableContainer.find(".floatThead-container").css({width: newWidth});
+							tableContainer.find(".floatThead-container table.floatThead-table").css({width: "100%"});
 						}, 1000);
 					})
 
