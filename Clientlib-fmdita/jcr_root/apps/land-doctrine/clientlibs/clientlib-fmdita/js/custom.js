eb95bbc587f2cc41bae31c2f1703e01956bfe2c2
JessicaJeong <jessica.jeong@growthops.com.au>
2020-06-05 21:43:02 +1000

    fix getCachedScript removal

--- Clientlib-fmdita/jcr_root/apps/land-doctrine/clientlibs/clientlib-fmdita/js/custom.js
+++ Clientlib-fmdita/jcr_root/apps/land-doctrine/clientlibs/clientlib-fmdita/js/custom.js
@@ -2,7 +2,7 @@ var isIE = /*@cc_on!@*/ false || !!document.documentMode;
 var isEdge = !isIE && !!window.StyleMedia;
 if (isEdge || isIE) {
     $('.toccontainer').addClass("toccontainer-top-ie");
-    $(window).scroll(function(event) {
+    $(window).scroll(function (event) {
         var scroll = $(window).scrollTop();
         // Activate sticky for IE and Edge if scrolltop is more than content top
         var contentHeight = $('.article-content').height();
@@ -13,16 +13,20 @@ if (isEdge || isIE) {
             if (scroll > (contentHeight + 70)) {
                 $('.toccontainer').removeClass("toccontainer-top-ie-scroll");
                 var topToc = contentHeight - 150;
-                $('.toccontainer').css({ 'top': topToc + 'px' });
+                $('.toccontainer').css({
+                    'top': topToc + 'px'
+                });
             } else {
 
-                $('.toccontainer').css({ 'top': 0 + 'px' });
+                $('.toccontainer').css({
+                    'top': 0 + 'px'
+                });
                 $('.toccontainer').removeClass("toccontainer-top-ie-scroll");
             }
         }
 
     });
-    $("div.topic ol").each(function() {
+    $("div.topic ol").each(function () {
         var $this = $(this);
         var startValue = $this.attr('start');
         if (typeof startValue !== typeof undefined && startValue !== false) {
@@ -30,8 +34,8 @@ if (isEdge || isIE) {
             var counterValue = $this.attr('data-outputclass');
             if (!(typeof styleValue !== typeof undefined && styleValue !== false && styleValue.indexOf("counter-increment:") > -1) &&
                 !(typeof counterValue !== typeof undefined && counterValue !== false && counterValue.indexOf("noChapterNumbering") > -1)) {
-                    $this.attr("data-customie", "customparanumber");
-                    $this.attr("style", "counter-increment: my-counter " + (parseInt(startValue) - 1) + ";");
+                $this.attr("data-customie", "customparanumber");
+                $this.attr("style", "counter-increment: my-counter " + (parseInt(startValue) - 1) + ";");
             }
         }
     });
@@ -40,8 +44,8 @@ if (isEdge || isIE) {
 } else {
     $('.toccontainer').addClass("toccontainer-top-all");
 }
-$(document).ready(function() {
-    $("div,td,section").each(function() {
+$(document).ready(function () {
+    $("div,td,section").each(function () {
         var $this = $(this);
         var attr = $this.attr('data-outputclass');
         if (typeof attr !== typeof undefined && attr !== false) {
@@ -58,7 +62,7 @@ $(document).ready(function() {
         }
 
     });
-    $("colgroup").each(function() {
+    $("colgroup").each(function () {
         var $this = $(this);
         if ($this.hasClass('FormatB')) {
             $this.parent().addClass('FormatB');
@@ -72,75 +76,60 @@ $(document).ready(function() {
 
     });
 
-    $("th").each(function() {
+    $("th").each(function () {
         var $this = $(this);
         if ($this.attr('rotate') == 'yes') {
             $this.find('.p').addClass('rotate-text');
         }
     });
 
-    $("object").each(function() {
+    $("object").each(function () {
         //console.log("object");
         //console.log($(this).attr('data'));
         var $this = $(this);
         var fileName = $this.attr('data');
         var formatCheck = '.mp4';
         if (fileName.indexOf(formatCheck) != -1) {
-            $this.css({ "width": "100%", "height": "400px" });
+            $this.css({
+                "width": "100%",
+                "height": "400px"
+            });
         }
 
     });
 
     /* fix for replacing nbsp with wbr */
 
-    $("div.topic div.p").html(function(i, html) {
+    $("div.topic div.p").html(function (i, html) {
         return html.replace(/&nbsp;/g, ' <wbr> ');
     });
-	
-	/* styling of tooltip for glossary based on character length */
-	$("body").find(".tooltipdefinition").each(function(){
+
+    /* styling of tooltip for glossary based on character length */
+    $("body").find(".tooltipdefinition").each(function () {
         var $this = $(this);
-        if($this.text().length > 13){
+        if ($this.text().length > 13) {
             $this.css("min-width", "150px");
         }
-        if($this.text().length > 35){
+        if ($this.text().length > 35) {
             $this.css("min-width", "200px");
         }
-        if($this.text().length > 50){
+        if ($this.text().length > 50) {
             $this.css("min-width", "250px");
         }
     });
 
     // SOURABH START
 
-    // Setup script import and cache it
-    $.cachedScript = function(url, options) {
-        options = $.extend(options || {}, {
-            dataType: "script",
-            cache: true,
-            url: url
-        });
-
-        // Use $.ajax() since it is more flexible than $.getScript
-        // Return the jqXHR object so we can chain callbacks
-        return $.ajax(options);
-    };
-
-    // import perfect scrollbar
-    $.cachedScript("/apps/land-doctrine/clientlibs/clientlib-fmdita/js/perfect-scrollbar.js").done(function(script, textStatus) {
-        // console.log("scrollbar " + textStatus);
-    });
-
     var hash = window.location.hash;
     var itemId = hash.substring(1, hash.length);
     var targetEl = document.getElementById(itemId);
-    if($(targetEl).closest("table").hasClass("wheading")){
+    if ($(targetEl).closest("table").hasClass("wheading")) {
         $("html").animate({
             scrollTop: 0
         }, 0);
 
-        setTimeout(function() {
-            if (hash != '') { 
+        setTimeout(function () {
+            if (hash != '') {
                 var x = $(targetEl).offset();
                 $("html").animate({
                     scrollTop: x.top - 50
@@ -148,10 +137,10 @@ $(document).ready(function() {
             }
         }, 300);
 
-    }else{
+    } else {
 
-        setTimeout(function() {
-            if (hash != '') { 
+        setTimeout(function () {
+            if (hash != '') {
                 var x = $(targetEl).offset();
                 $("html").animate({
                     scrollTop: x.top
@@ -160,67 +149,64 @@ $(document).ready(function() {
         }, 300);
     }
 
-    setTimeout(function() {
+    $(".table-scroll-container table").each(function () {
+        var table = $(this);
+        var tableContainer = table.closest('.table-container');
+        var tableScrollContainer = table.closest('.table-scroll-container');
 
-        // import FloatHeadJS (the cdn version is used for override testing, do not remove)
-        // $.cachedScript("https://cdnjs.cloudflare.com/ajax/libs/floatthead/2.1.4/jquery.floatThead.min.js").done(function(script, textStatus) {
-		$.cachedScript("/apps/land-doctrine/clientlibs/clientlib-fmdita/js/floaTheadPlugin.js").done(function(script, textStatus) {
-            // console.log("floathead " + textStatus);
-            $(".table-scroll-container table").each(function() {
-                var table = $(this);
-                var tableContainer = table.closest('.table-container');
-                var tableScrollContainer = table.closest('.table-scroll-container');
-                
-                table.on("floatThead", function() {
-                    var newWidth = tableScrollContainer.width();
-                    tableContainer.find(".floatThead-container").css({ width: newWidth });
-                })
-                table.on("reflowed", function() {
-                    setTimeout(function() {
-                        var newWidth = tableScrollContainer.width();
-                        tableContainer.find(".floatThead-container").css({ width: newWidth });
-                        tableContainer.find(".floatThead-container table.floatThead-table").css({ width: "100%" });
-                    }, 1000);
-                })
-
-                if (table.height() < tableScrollContainer.height()) {
-                    tableScrollContainer.addClass("table-scroll-container--x-only");
-                    // no floathead for these small tables
-                } else {
-                    table.floatThead({
-                        top: 0,
-                        scrollContainer: function(table) {
-                            return table.closest('.table-scroll-container');
-                        },
-                        position: 'absolute'
-                    });
-                }
-                table.floatThead('reflow');
-                if (tableScrollContainer.hasClass("ps-container")) {
-                    tableScrollContainer.perfectScrollbar('update');
-                } else {
-                    tableScrollContainer.perfectScrollbar();
-                }
-
-            });
-
-            $(".floatThead-wrapper").each(function(e) {
-                var newWidth = $(".table-scroll-container").width();
-                $(this).find(".floatThead-container").css({ width: newWidth });
-            });
-
-            $(document).on("click", ".floatThead-wrapper .thead, .fixed-btn", function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-                var targetTableBody = $(this).closest(".expandable-table");
-                targetTableBody.find(".tbody").click();
+        table.on("floatThead", function () {
+            var newWidth = tableScrollContainer.width();
+            tableContainer.find(".floatThead-container").css({
+                width: newWidth
             });
         })
+        table.on("reflowed", function () {
+            setTimeout(function () {
+                var newWidth = tableScrollContainer.width();
+                tableContainer.find(".floatThead-container").css({
+                    width: newWidth
+                });
+                tableContainer.find(".floatThead-container table.floatThead-table").css({
+                    width: "100%"
+                });
+            }, 1000);
+        })
 
+        if (table.height() < tableScrollContainer.height()) {
+            tableScrollContainer.addClass("table-scroll-container--x-only");
+            // no floathead for these small tables
+        } else {
+            table.floatThead({
+                top: 0,
+                scrollContainer: function (table) {
+                    return table.closest('.table-scroll-container');
+                },
+                position: 'absolute'
+            });
+        }
+        table.floatThead('reflow');
+        if (tableScrollContainer.hasClass("ps-container")) {
+            tableScrollContainer.perfectScrollbar('update');
+        } else {
+            tableScrollContainer.perfectScrollbar();
+        }
 
+    });
 
+    $(".floatThead-wrapper").each(function (e) {
+        var newWidth = $(".table-scroll-container").width();
+        $(this).find(".floatThead-container").css({
+            width: newWidth
+        });
+    });
 
-    }, 1200);
+    $(document).on("click", ".floatThead-wrapper .thead, .fixed-btn", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var targetTableBody = $(this).closest(".expandable-table");
+        targetTableBody.find(".tbody").click();
+    });
+
 
 });
 
